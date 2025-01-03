f665209506cd8a686aa21d37d7d3aa7fa9ea6f46
Hitesh Mulwani <hiteshmulwanieng@gmail.com>
2025-01-03 06:25:48 +0530

    L2: variables & data types

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(String[] args) {
+        byte b=3;
+        short s=129;
+        int i=5532;
+        long l=324352l;
+
+        float f=5.2f;
+        double d=3243.23432;
+
+        char c='h';
+        
+        boolean bool=true;
+    }
+}
