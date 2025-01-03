815b70cd3e464a2e0581d07aed472f06ca764381
Hitesh Mulwani <hiteshmulwanieng@gmail.com>
2025-01-03 11:21:11 +0530

    L5: relational operators

---
+++ relational.java
@@ -0,0 +1,12 @@
+public class relational {
+    public static void main(String[] args) {
+        int x=6;
+        int y=5;
+        boolean small= x<y;
+        System.out.println(small);
+        boolean big=x>y;
+        System.out.println(big);
+        boolean notEqual= x!=y;
+        System.out.println(notEqual);
+    }
+}
