e92205ca5bd1c6ed64d0b5c66bae9f4dc7577a27
Hitesh Mulwani <hiteshmulwanieng@gmail.com>
2025-01-03 11:34:51 +0530

    L6: logical operators

---
+++ logical.java
@@ -0,0 +1,12 @@
+public class logical {
+    public static void main(String[] args) {
+        int x=7;
+        int y=5;
+        int a=5;
+        int b=9;
+
+        boolean result= (x>y || a<b);
+        System.out.println(result);
+        System.out.println(!(a>b));
+    }
+}
